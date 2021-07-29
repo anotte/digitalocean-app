c6da64f3521608c9619b4791a0a5b388d2305dcc
anotte <notte@ewti.com.br>
2021-07-29 20:51:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    next();
+});
+
+// All OPTIONS requests return a simple status: 'OK'
+app.options('*', (req, res) => {
+    res.json({
+        status: 'OK'
+    });
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/callmesilent', (req, res) => res.send('Hello World!'))
