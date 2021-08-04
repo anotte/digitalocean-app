bce9e9aaa44fb9857472ece54dad06b5f537a7d3
anotte <notte@ewti.com.br>
2021-08-04 11:50:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const fs = require('fs')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
